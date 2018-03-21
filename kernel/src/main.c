248a93f1838e974f3d6d1c57fc01e2cc55c26516
BuildTools <unconfigured@null.spigotmc.org>
2018-03-21 09:28:32 +0100

    Cosmetics
    Change branding :-)
    add version number header

--- kernel/src/main.c
+++ kernel/src/main.c
@@ -8,15 +8,12 @@
 #include "cpu_state.h"
 #include "pci.h"
 #include "util.h"
-
+#include "version.h"
 
 void kernel_main(struct multiboot_mbs_info * mbs_info) 
 {
 	vk_setup_io(term_putc, term_puts);
 	term_init();
-
-	term_setcolor(14, 0);
-	vk_printf("vloOS 0.0.1337 Kernel\n");
 	
 	term_setcolor(15, 0);
 
@@ -35,12 +32,13 @@ void kernel_main(struct multiboot_mbs_info * mbs_info)
 
 	testtasks_init(mbs_info);
 
+	term_setcolor(14, 0);
+	vk_printf("welcome to vlOS version %s\n", VERSION);
 	vk_printf("keys:\n1 - 6 : Toggle test tasks (multiboot elf modules)\n");
 	vk_printf("7     : Show physical memory map\n");
 	vk_printf("8     : process list / stats\n");
-	vk_printf("enabling interrupts and just WALK AWAY\n");
-	
 	term_setcolor(7, 0);
+	
 	interrupt_enable();
 	
 	
